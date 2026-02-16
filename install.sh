f67827955b30419a94c30bf43f7f11061dfd39eb
Ganesh-Surya-Manimohan <surya.manimohan@gmail.com>
2026-02-17 00:47:43 +0530

    docker instance

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ dnf -y install dnf-plugins-core
 dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 dnf -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 systemctl enable --now docker
+systemctl start docker
+usermod -aG docker ec2-user
