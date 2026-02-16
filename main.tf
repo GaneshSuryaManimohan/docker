3d3ab1d4dd47243ff63e92cc47e97b05f2549705
Ganesh-Surya-Manimohan <surya.manimohan@gmail.com>
2026-02-17 00:39:59 +0530

    docker instance

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "docker" {
+        ami = "ami-0220d79f3f480ecf5"
+        instance_type = "t3.micro"
+        vpc_security_group_ids = ["sg-0bbdd2b154434fbfd"]
+        user_data = file("install.sh")
+    tags = {
+        Name = "Docket"
+        CreatedBy = "Terraform"
+    }
+}
