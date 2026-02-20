7df5dea885e8d06f46bf7bd560459bd1fdb28f8e
Ganesh-Surya-Manimohan <surya.manimohan@gmail.com>
2026-02-21 00:45:12 +0530

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 set -e
-
+R="e\[31m"
+N="e\[0m"
 dnf -y install dnf-plugins-core
 dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 dnf -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 systemctl enable --now docker
 systemctl start docker
 usermod -aG docker ec2-user
-newgrp docker
+#newgrp docker
+echo -e "$R Logout and Login again to apply the new group membership $N"
